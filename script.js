e898111da18822eede8830f7ed72a420bb21d963
AdrianK-B06 <adriank.b06.kodehode@gmail.com>
2023-09-28 17:25:12 +0200

    Added basic JS functions, some html and css changes

---
+++ script.js
@@ -0,0 +1,101 @@
+
+const redPlayerBox = document.querySelector(".redPlayer")
+const bluePlayerBox = document.querySelector(".bluePlayer")
+const winnerBox = document.querySelector("#result")
+
+const gameElements = ["✊🏻" , "✋🏻" , "✌🏻"];
+
+
+// Initialize variables to track player choices and animation intervals
+let redPlayerChoice = null; // Stores the choice of the red player
+let bluePlayerChoice = null; // Stores the choice of the blue player
+
+let redPlayerInterval; // animations interval for red player
+let bluePlayerInterval; // animations interval for blue player
+
+let processingKey = false; // Boolean to indicate if key is pressed
+
+
+// Listen for keydown events to start player animations 
+
+document.addEventListener("keydown", (event) => {
+    if (processingKey) {
+        return;
+    }
+    if (event.key === "f" && redPlayerChoice === null) {
+        processingKey = true;
+        redPlayerInterval = setInterval(() => {
+            redPlayerBox.textContent = getRandomElement(gameElements);
+        }, 100);
+    } else if (event.key === "j" && bluePlayerChoice === null) {
+        processingKey = true;
+        bluePlayerInterval = setInterval(() => {
+            bluePlayerBox.textContent = getRandomElement(gameElements);
+        }, 100);
+    }
+});
+
+
+// Listen for keyup events to stop player animations and determine the winner
+
+document.addEventListener("keyup", (event) => {
+    if (event.key === "f" && redPlayerChoice === null) {
+        processingKey = true; 
+        clearInterval(redPlayerInterval);
+        redPlayerChoice = getRandomElement(gameElements);
+        redPlayerBox.textContent = redPlayerChoice;
+        determineWinner();
+        processingKey = false;
+
+    }   else if (event.key === "j" && bluePlayerChoice === null) {
+        clearInterval(bluePlayerInterval);
+        bluePlayerChoice = getRandomElement(gameElements);
+        bluePlayerBox.textContent = bluePlayerChoice;
+        determineWinner();
+        processingKey = false;
+    }
+
+// Event listener for the spacebar key to reset the game    
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === " ") {
+        resetGame();
+    }
+})
+});
+
+// Function to get random element from array
+
+function getRandomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+
+// Function to determine the winner based on the game rules
+
+function determineWinner() {
+    if (redPlayerChoice && bluePlayerChoice) {
+        if (redPlayerChoice == bluePlayerChoice) {
+            winnerBox.textContent = "It's a tie!";
+        } else if (
+            (redPlayerChoice === "✊🏻" && bluePlayerChoice === "✌🏻") ||
+            (redPlayerChoice === "✋🏻" && bluePlayerChoice === "✊🏻") ||
+            (redPlayerChoice === "✌🏻" && bluePlayerChoice === "✋🏻")
+        )   {
+            winnerBox.textContent = "Red Team Wins!";
+        } else {
+            winnerBox.textContent = "Blue Team Wins!";
+        }
+    }
+}
+
+function resetGame() {
+    redPlayerChoice = null;
+    bluePlayerChoice = null;
+    clearInterval(redPlayerInterval);
+    clearInterval(bluePlayerInterval);
+
+    redPlayerBox.textContent = " ";
+    bluePlayerBox.textContent = " ";
+    winnerBox.textContent = "VS";
+}
